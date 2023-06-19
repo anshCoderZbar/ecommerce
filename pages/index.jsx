a0dd2ca9c6163cdb6955c0237ae672bfd8c426a7
anshCoderZbar <ansh.coderzbar@gmail.com>
2023-06-19 17:24:21 +0530

    added react-spring

--- pages/index.jsx
+++ pages/index.jsx
@@ -44,29 +44,31 @@ export default function Home() {
             );
           })}
         </Swiper>
-        <div className="container mx-auto">
-          <div className="flex flex-wrap gap-5 md:gap-7 lg:gap-10 items-center justify-center py-5">
-            {Section?.map((data, i) => {
-              return (
-                <animated.div
-                  style={props}
-                  key={i}
-                  className="content relative"
-                >
-                  <Image
-                    src={data?.img}
-                    alt={data?.heading}
-                    loading="lazy"
-                    className="rounded-md w-28 shadow-lg h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 "
-                  />
-                  <div className="overlay absolute bg-red-400 rounded-md bottom-0 left-0 right-0 overflow-hidden h-0 transition-all duration-300">
-                    <div className="text text-white  text-sm md:text-xl absolute overflow-hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 capitalize">
-                      {data?.heading}
+        <div className="bg-white dark:bg-gray-900">
+          <div className="container mx-auto ">
+            <div className="flex flex-wrap gap-5 md:gap-7 lg:gap-10 items-center justify-center py-5 ">
+              {Section?.map((data, i) => {
+                return (
+                  <animated.div
+                    style={props}
+                    key={i}
+                    className="content relative"
+                  >
+                    <Image
+                      src={data?.img}
+                      alt={data?.heading}
+                      loading="lazy"
+                      className="rounded-md w-28 shadow-lg h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 "
+                    />
+                    <div className="overlay absolute bg-red-400 rounded-md bottom-0 left-0 right-0 overflow-hidden h-0 transition-all duration-300 dark:bg-red-950">
+                      <div className="text text-white  text-sm md:text-xl absolute overflow-hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 capitalize">
+                        {data?.heading}
+                      </div>
                     </div>
-                  </div>
-                </animated.div>
-              );
-            })}
+                  </animated.div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </Layout>
