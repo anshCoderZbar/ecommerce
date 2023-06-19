ed775e403319c543ec0b3f5a171e3170f5882a1b
anshCoderZbar <ansh.coderzbar@gmail.com>
2023-06-19 15:42:28 +0530

    added banners

---
+++ mock/section/index.js
@@ -0,0 +1,32 @@
+export const Section = [
+  {
+    id: 1,
+    img: require("app/assets/banner/logo1.jpg"),
+    heading: "men",
+  },
+  {
+    id: 2,
+    img: require("app/assets/banner/logo2.jpg"),
+    heading: "women",
+  },
+  {
+    id: 3,
+    img: require("app/assets/banner/logo3.jpg"),
+    heading: "girl",
+  },
+  {
+    id: 4,
+    img: require("app/assets/banner/logo4.jpg"),
+    heading: "boy",
+  },
+  {
+    id: 5,
+    img: require("app/assets/banner/logo5.jpg"),
+    heading: "beauty",
+  },
+  {
+    id: 6,
+    img: require("app/assets/banner/logo6.jpg"),
+    heading: "accessories",
+  },
+];
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,9 +4,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import banner1 from "app/assets/banner/banner1.jpg";
 import { BannerImg } from "mock/banner";
+import logo1 from "app/assets/banner/logo1.jpg";
+import { Section } from "mock/section";
 
 export default function Home() {
   return (
@@ -33,6 +35,27 @@ export default function Home() {
             );
           })}
         </Swiper>
+        <div className="container mx-auto">
+          <div className="flex flex-wrap gap-5 md:gap-7 lg:gap-10 items-center justify-center py-5">
+            {Section?.map((data, i) => {
+              return (
+                <div key={i} class="content relative">
+                  <Image
+                    src={data?.img}
+                    alt={data?.heading}
+                    loading="lazy"
+                    className="rounded-md w-28 shadow-lg h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 "
+                  />
+                  <div class="overlay absolute bg-red-400 rounded-md bottom-0 left-0 right-0 overflow-hidden h-0 transition-all duration-300">
+                    <div class="text text-white text-xl absolute overflow-hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 capitalize">
+                      {data?.heading}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </Layout>
     </main>
   );
