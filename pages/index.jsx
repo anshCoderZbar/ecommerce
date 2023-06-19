c7b5eb9dd7d3a4f28d11a19f45742bdcbaf9d41f
anshCoderZbar <ansh.coderzbar@gmail.com>
2023-06-19 17:06:57 +0530

    added react-spring

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,12 +5,21 @@ import { Navigation, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { animated, useSpring } from "@react-spring/web";
 
 import { BannerImg } from "mock/banner";
-import logo1 from "app/assets/banner/logo1.jpg";
 import { Section } from "mock/section";
 
 export default function Home() {
+  const [props, api] = useSpring(
+    () => ({
+      from: { transform: "scale(0)", opacity: 0 },
+      to: { transform: "scale(1)", opacity: 1 },
+      immediate: false,
+      config: { duration: 1000 },
+    }),
+    []
+  );
   return (
     <main>
       <Layout>
@@ -39,19 +48,23 @@ export default function Home() {
           <div className="flex flex-wrap gap-5 md:gap-7 lg:gap-10 items-center justify-center py-5">
             {Section?.map((data, i) => {
               return (
-                <div key={i} class="content relative">
+                <animated.div
+                  style={props}
+                  key={i}
+                  className="content relative"
+                >
                   <Image
                     src={data?.img}
                     alt={data?.heading}
                     loading="lazy"
                     className="rounded-md w-28 shadow-lg h-28 sm:w-32 sm:h-32 md:w-36 md:h-36 lg:w-40 lg:h-40 "
                   />
-                  <div class="overlay absolute bg-red-400 rounded-md bottom-0 left-0 right-0 overflow-hidden h-0 transition-all duration-300">
-                    <div class="text text-white text-xl absolute overflow-hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 capitalize">
+                  <div className="overlay absolute bg-red-400 rounded-md bottom-0 left-0 right-0 overflow-hidden h-0 transition-all duration-300">
+                    <div className="text text-white text-xl absolute overflow-hidden top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 capitalize">
                       {data?.heading}
                     </div>
                   </div>
-                </div>
+                </animated.div>
               );
             })}
           </div>
