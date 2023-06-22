c9db919be4ad7426d576b6f02822286940bd9277
anshCoderZbar <ansh.coderzbar@gmail.com>
2023-06-22 18:20:08 +0530

    learn tupple and enum

--- app/common/men/MenFilter.jsx
+++ app/common/men/MenFilter.jsx
@@ -15,8 +15,8 @@ export const MenFilter = ({ active, setActive }) => {
     <div
       className={`${
         active
-          ? "-translate-x-0 transition-all"
-          : "-translate-x-full transition-all"
+          ? "-translate-x-0 transition-all !overflow-y-auto"
+          : "-translate-x-full transition-all overflow-hidden"
       }  absolute h-full w-[90%] sm:9/12 md:w-1/2 lg:w-4/12	 left-0 xl:w-80 xl:h-auto xl:left-auto bg-white dark:bg-dark-background xl:translate-x-0 xl:static`}
     >
       <div className="w-full relative py-6 rounded-lg  mb-6 pb-6">
