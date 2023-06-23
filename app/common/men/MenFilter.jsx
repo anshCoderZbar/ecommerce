05905d2081bd2281274ebe0f2ab509c8df2d7287
anshCoderZbar <ansh.coderzbar@gmail.com>
2023-06-23 09:27:55 +0530

    learn tupple and enum

--- app/common/men/MenFilter.jsx
+++ app/common/men/MenFilter.jsx
@@ -17,13 +17,13 @@ export const MenFilter = ({ active, setActive }) => {
         active
           ? "-translate-x-0 transition-all !overflow-y-auto"
           : "-translate-x-full transition-all overflow-hidden"
-      }  absolute h-full w-[90%] sm:9/12 md:w-1/2 lg:w-4/12	 left-0 xl:w-80 xl:h-auto xl:left-auto bg-white dark:bg-dark-background xl:translate-x-0 xl:static`}
+      }  absolute h-screen w-[90%] sm:9/12 md:w-1/2 lg:w-4/12	 left-0 xl:w-80 xl:h-auto xl:left-auto bg-white dark:bg-dark-background xl:translate-x-0 xl:static`}
     >
       <div className="w-full relative py-6 rounded-lg  mb-6 pb-6">
         {active && (
           <div
             onClick={() => setActive(false)}
-            className="absolute -right-8 border top-0 text-3xl rounded-tr-md rounded-br-md border-black dark:border-white xl:hidden"
+            className="absolute right-0 border top-2 text-3xl rounded-md border-black dark:border-white xl:hidden"
           >
             <RxCross2 />
           </div>
