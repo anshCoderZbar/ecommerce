2a5f8b2239e5aefd783655dd5070cb41425838a7
anshCoderZbar <ansh.coderzbar@gmail.com>
2023-06-27 18:23:23 +0530

    corrected the layout

--- mock/men/Filter.js
+++ mock/men/Filter.js
@@ -65,4 +65,70 @@ export const MenFilterData = [
       },
     ],
   },
+  {
+    id: 1,
+    name: "category",
+    filters: [
+      {
+        id: 1,
+        name: "jeans",
+        slug: "/jeans",
+      },
+      {
+        id: 2,
+        name: "t-shirt",
+        slug: "/t-shirt",
+      },
+      {
+        id: 3,
+        name: "shirt",
+        slug: "/t-shirt",
+      },
+      {
+        id: 4,
+        name: "shoes",
+        slug: "/shoes",
+      },
+      {
+        id: 5,
+        name: "sweatshirt",
+        slug: "/sweatshirt",
+      },
+      {
+        id: 6,
+        name: "sweatshirt",
+        slug: "/sweatshirt",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Price-range",
+    filters: [
+      {
+        name: "Under ₹50",
+        minPrice: 0,
+        maxPrice: 50,
+        slug: "/50",
+      },
+      {
+        name: "₹50 - ₹100",
+        minPrice: 50,
+        maxPrice: 100,
+        slug: "/50-100",
+      },
+      {
+        name: "₹100 - ₹200",
+        minPrice: 100,
+        maxPrice: 200,
+        slug: "/100-200",
+      },
+      {
+        name: "Over ₹200",
+        minPrice: 200,
+        maxPrice: Infinity,
+        slug: "/200",
+      },
+    ],
+  },
 ];
