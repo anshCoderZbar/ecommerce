526609a5820d60b989748f527789a68c2c27e344
anshCoderZbar <ansh.coderzbar@gmail.com>
2023-06-29 10:33:27 +0530

    remove push notification

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,8 @@
-import Head from "next/head";
 import Router from "next/router";
 import NProgress from "nprogress";
 
 import { ThemeProvider } from "next-themes";
 import "/styles/globals.scss";
-import { useEffect } from "react";
-import Script from "next/script";
 
 Router.onRouteChangeStart = (url) => {
   NProgress.start();
@@ -16,35 +13,9 @@ Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeError", () => () => NProgress.done());
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function () {
-      OneSignal.init({
-        appId: "fc724930-44c6-4875-bbd0-1a2e77032fce",
-        notifyButton: {
-          enable: true,
-        },
-        allowLocalhostAsSecureOrigin: true,
-      });
-    });
-    return () => {
-      window.OneSignal = undefined;
-    };
-  }, []);
-
   return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-        />
-        <title> Next.js</title>
-        <Script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" defer />
-      </Head>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
